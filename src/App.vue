5a902f71df6a0b2c7a5c7e0d4d9b225e179609f0
Joseph Essel Moses <jmosesessel21@gmail.com>
2022-08-25 14:59:34 +0000

    added SideNavigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SideNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
   import { RouterView } from 'vue-router';
+  import SideNavigation from './components/SideNavigation.vue'
 </script>
 
 <style lang="scss" scoped>
