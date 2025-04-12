1b7559e9332e0dd4604e494523dad1919664e1b0
Joseph Essel Moses <jmosesessel21@gmail.com>
2025-04-12 14:22:51 -0400

    Update index.html and vite.config.js for deployment adjustments
    - Changed favicon path in index.html to reflect new directory structure.
    - Updated script and stylesheet paths in index.html for correct asset loading.
    - Commented out the alias resolution in vite.config.js for potential future use.

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/j-weather-app/',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 })
